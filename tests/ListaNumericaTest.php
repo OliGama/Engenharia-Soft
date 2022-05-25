cdf35ff61e7d72bf968016cc0583e5942a9ed07c
OliGama <102246413+OliGama@users.noreply.github.com>
2022-05-24 22:09:52 -0300

    Update ListaNumericaTest.php

--- tests/ListaNumericaTest.php
+++ tests/ListaNumericaTest.php
@@ -9,57 +9,31 @@ class ListaNumericaTest extends TestCase{
      * @dataProvider additionProvider
      */
 
-    /*public function testVerificaQuantidadeValores($vet, $expected){
-        $ln = new ListaNumerica;
-        $ln->setValores($vet);
-        $this->assertEquals($expected, $ln->verificaQuantidade());
-    }*/
+    // public function testVerificaQuantidadeValores($vet, $expected){
+    //     $ln = new ListaNumerica;
+    //     $ln->setValores($vet);
+    //     $this->assertEquals($expected, $ln->verificaQuantidade());
+    // }
 
-    public function testVerificaValores($vet, $expected){
+    // public function testVerificaValores($vet, $expected){
+    //     $ln = new ListaNumerica;
+    //     $ln->setValores($vet);
+    //     $this->assertEquals($expected, $ln->verificaTresPrimeiros());
+    // }
+
+    public function testVerificaUltimos($vet, $expected){
         $ln = new ListaNumerica;
         $ln->setValores($vet);
-        $this->assertEquals($expected, $ln->verificaValor());
+        $this->assertEquals($expected, $ln->VerificaUltimos());
     }
 
     public function additionProvider() : array{
         return[
-            'Erro 9999' => [[],'Invalido'],
-            'Erro 1000000' => [[1000000],'Invalido'],
-            'Certo 11111' => [[11111],'Valido'],
-            'Certo 20000' => [[20000],'Valido'],
+            'teste 1' => ['aaa7777','Valido'],
+            'teste 2' => ['a2a8888','Invalido'],
+            'teste 3' => ['aaa55a5','Valido'],
+            'teste 4' => ['','Invalido'],
         ];
     }
-
-    /*
-    public function testVerificaQuantidadeValoresEntradaVazio(){
-        $ln = new ListaNumerica;
-        $ln->setValores([]);
-        $this->assertEquals('Invalido', $ln->verificaQuantidade());
-    }
-    public function testVerificaQuantidadeValoresEntradaTres(){
-        $ln = new ListaNumerica;
-        $ln->setValores([44141,41414,41141]);
-        $this->assertEquals('Invalido', $ln->verificaQuantidade());
-    }
-    public function testVerificaQuantidadeValoresEntradaQuatro(){
-        $ln = new ListaNumerica;
-        $ln->setValores([44141,41414,41141,45544]);
-        $this->assertEquals('Valido', $ln->verificaQuantidade());
-    }
-    public function testVerificaQuantidadeValoresEntradaoOnze(){
-        $ln = new ListaNumerica;
-        $ln->setValores([44141,41414,41141,44141,41414,41141,44141,41414,41141,44141,41414]);
-        $this->assertEquals('Invalido', $ln->verificaQuantidade());
-    }
-    public function testVerificaQuantidadeValoresEntradaDez(){
-        $ln = new ListaNumerica;
-        $ln->setValores([44141,41414,41141,44141,41414,41141,44141,41414,41141,44141]);
-        $this->assertEquals('Valido', $ln->verificaQuantidade());
-    }
-    public function testVerificaQuantidadeValoresEntradaSeis(){
-        $ln = new ListaNumerica;
-        $ln->setValores([44141,41414,41141,44141,41414,41141]);
-        $this->assertEquals('Valido', $ln->verificaQuantidade());
-    }*/
 }
 ?>
